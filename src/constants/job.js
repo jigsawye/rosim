296975f7d788bc6d50ef7a27c2a93e84ce728307
jigsawye <jigsaw.ye@gmail.com>
2018-02-14 15:39:49 +0800

    Add weapons data

--- src/constants/job.js
+++ src/constants/job.js
@@ -1,15 +1,8 @@
-export const KNT = 'Knight';
-export const ASS = 'Assassin';
-export const WIZ = 'Wizard';
-export const PRI = 'Priest';
-export const HNT = 'Hunter';
-export const BSM = 'Blacksmith';
-
 export const jobs = [
-  { id: 7, key: KNT, name: '騎士' },
-  { id: 8, key: PRI, name: '祭司' },
-  { id: 9, key: WIZ, name: '巫師' },
-  { id: 10, key: BSM, name: '鐵匠' },
-  { id: 11, key: HNT, name: '獵人' },
-  { id: 12, key: ASS, name: '刺客' },
+  { id: 7, name: '騎士' },
+  { id: 8, name: '祭司' },
+  { id: 9, name: '巫師' },
+  { id: 10, name: '鐵匠' },
+  { id: 11, name: '獵人' },
+  { id: 12, name: '刺客' },
 ];
---
+++ src/constants/weapons.js
@@ -0,0 +1,82 @@
+export const weapons = [
+  id: 0, name: '空手',
+  id: 1, name: '短劍',
+  id: 2, name: '單手劍',
+  id: 3, name: '雙手劍',
+  id: 4, name: '單手槍',
+  id: 5, name: '雙手槍',
+  id: 6, name: '單手斧',
+  id: 7, name: '雙手斧',
+  id: 8, name: '鈍器',
+  id: 9, name: '單手杖',
+  id: 10, name: '弓',
+  id: 11, name: '拳刃',
+  id: 12, name: '書',
+  id: 13, name: '拳套',
+  id: 14, name: '樂器',
+  id: 15, name: '鞭子',
+];
+
+export const weaponsBaseAspd = [
+  {
+    jobId: 7,
+    weapons: [
+      { id: 0, baseAspd: 154 },
+      { id: 1, baseAspd: 145 },
+      { id: 2, baseAspd: 149 },
+      { id: 3, baseAspd: 142 },
+      { id: 4, baseAspd: 139 },
+      { id: 5, baseAspd: 134 },
+      { id: 6, baseAspd: 144 },
+      { id: 7, baseAspd: 139 },
+      { id: 8, baseAspd: 149 },
+    ],
+  },
+  {
+    jobId: 8,
+    weapons: [
+      { id: 0, baseAspd: 154 },
+      { id: 8, baseAspd: 151 },
+      { id: 9, baseAspd: 134 },
+      { id: 12, baseAspd: 150 },
+      { id: 13, baseAspd: 134 },
+    ],
+  },
+  {
+    jobId: 9,
+    weapons: [
+      { id: 0, baseAspd: 144 },
+      { id: 1, baseAspd: 140 },
+      { id: 9, baseAspd: 141 },
+    ],
+  },
+  {
+    jobId: 10,
+    weapons: [
+      { id: 0, baseAspd: 154 },
+      { id: 1, baseAspd: 144 },
+      { id: 2, baseAspd: 144 },
+      { id: 6, baseAspd: 148 },
+      { id: 7, baseAspd: 141 },
+      { id: 8, baseAspd: 146 },
+    ],
+  },
+  {
+    jobId: 11,
+    weapons: [
+      { id: 0, baseAspd: 154 },
+      { id: 1, baseAspd: 141 },
+      { id: 10, baseAspd: 146 },
+    ],
+  },
+  {
+    jobId: 12,
+    weapons: [
+      { id: 0, baseAspd: 154 },
+      { id: 1, baseAspd: 152 },
+      { id: 2, baseAspd: 144 },
+      { id: 6, baseAspd: 143 },
+      { id: 11, baseAspd: 152 },
+    ],
+  },
+];
