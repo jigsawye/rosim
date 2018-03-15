490246645a7305e3fe72e6e659d2f14403e7b68d
jigsawye <jigsaw.ye@gmail.com>
2018-03-15 12:04:41 +0800

    Refactor hp/sp methods

--- src/utils/hpsp.js
+++ src/utils/hpsp.js
@@ -1,4 +1,4 @@
-import { round, floor, find } from 'lodash';
+import { round, floor, find, range } from 'lodash';
 import { compose, prop, includes } from 'lodash/fp';
 import { getJobType } from '../constants/classes';
 import { SECOND, THIRD } from '../constants/classes/classNames';
@@ -8,26 +8,16 @@ const findTable = (table, job) => find(table, compose(includes(job[1]), prop('jo
 
 const getSecondBaseHp = (baseLevel, job) => {
   const { hpJobA, hpJobB } = findTable(secondHpTable, job);
-  let baseHp = 35;
-
-  baseHp += baseLevel * hpJobB;
-
-  for (var i = 2; i <= baseLevel; i++) {
-    baseHp += round(hpJobA * i);
-  }
-
-  return baseHp;
-}
+  const baseHp = 35 + baseLevel * hpJobB;
+  return range(2, baseLevel + 1)
+    .reduce((currentBaseHp, level) => currentBaseHp + round(hpJobA * level), baseHp);
+};
 
 
 const getSecondBaseSp = (baseLevel, job) => {
   const { spJob } = findTable(secondSpTable, job);
-  let baseSp = 10;
-
-  baseSp += baseLevel * spJob;
-
-  return baseSp;
-}
+  return 10 + baseLevel * spJob;
+};
 
 const getThirdBaseHp = (baseLevel, job) => {
   const { hpTable } = findTable(thirdHpTable, job);
@@ -45,11 +35,7 @@ export const getMaxHp = (baseLevel, job, vit, { hpAddMod, hpMultiMod }) => {
   const transMod = type === SECOND ? 1 : 1.25;
   const getBaseHp = type === THIRD ? getThirdBaseHp : getSecondBaseHp;
   const baseHp = getBaseHp(baseLevel, job);
-
-  let maxHp = floor(baseHp * (1 + vit * 0.01) * transMod);
-
-  maxHp += hpAddMod;
-
+  const maxHp = floor(baseHp * (1 + vit * 0.01) * transMod) + hpAddMod;
   return floor(maxHp * (1 + hpMultiMod * 0.01));
 };
 
@@ -58,11 +44,6 @@ export const getMaxSp = (baseLevel, job, int, { spAddMod, spMultiMod }) => {
   const transMod = type === SECOND ? 1 : 1.25;
   const getBaseSp = type === THIRD ? getThirdBaseSp : getSecondBaseSp;
   const baseSp = getBaseSp(baseLevel, job);
-
-  let maxSp = floor(baseSp * (1 + int * 0.01));
-  maxSp = floor(maxSp * transMod);
-
-  maxSp += spAddMod;
-
+  const maxSp =floor(floor(baseSp * (1 + int * 0.01)) * transMod) + spAddMod;
   return floor(maxSp * (1 + spMultiMod * 0.01));
 };
